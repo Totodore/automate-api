1bf9ebf0dadb1afc7df3f8349bfee8157b85f527
Elariondakta <prevottheodore@gmail.com>
2020-05-13 16:09:34 +0200

    guildRes fix

--- routes/index.js
+++ routes/index.js
@@ -18,11 +18,16 @@ router.get('/', async (req, res, next) => {
     res.render('index', {header: req.headerData, error: "I didn't manage to collect all your channels, sniffu..."});
   }
   let guildRes = JSON.parse(await guildReq.text());
-  guildRes = guildRes.filter((el) => {
-    if (el.permissions & ADMINISTRATOR || el.permissions & MANAGE_GUILD)
-      return true;
-    else return false;
-  });
+  try {
+    guildRes = guildRes.filter((el) => {
+      if (el.permissions & ADMINISTRATOR || el.permissions & MANAGE_GUILD)
+        return true;
+      else return false;
+    });
+  } catch (e) {
+    console.error(`Erreur lors de la guildRes.filer : guildResObj : ${guildsRes}`);
+    res.render('index', {header: req.headerData, error: "I didn't manage to collect all your channels, sniffu..."});
+  }
   guildRes.forEach(element => element.added = fs.existsSync(__dirname+"/../data/guilds/"+element.id+"/data.json"));
   guildRes.sort((a, b) => {if (a.added) return -1; else if (b.added) return 1; else return 0;});
   if (guildRes.length > 0) 
