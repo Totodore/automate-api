bcb1b7b59aeddf0afe651c98ee840acf38edd272
Elariondakta <prevottheodore@gmail.com>
2020-05-24 21:24:35 +0200

    remove test

--- test/removeTooMuchMessages.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-//Supprime des messages si il y en a au dessus de 5 par guild
-
-fs.readdirSync(__dirname+"/../data/guilds/").forEach(guildId => {
-    const data = JSON.parse(fs.readFileSync(__dirname + "/../data/guilds/" + guildId + "/data.json"));
-    data.freq = data.freq.filter((el, index) => el.cron == "* * * * *" ? false : true);
-});
