9cbe7b8fa9ca65c092038d3ee9b9038724accfa3
Elariondakta <prevottheodore@gmail.com>
2020-10-07 10:55:34 +0200

    data model for guild and message

--- src/models/GuildModel.ts
+++ src/models/GuildModel.ts
@@ -1,5 +1,5 @@
-import { Guild } from "discord.js";
-import Model from "sequelize/types/lib/model";
+import {Model, DataTypes, Sequelize} from "sequelize";
+import { SequelizeAttributes } from "src/types";
 
 interface GuildDataModel {
     ponctual: string[] //List of Id which refer to MessagesModel
@@ -24,6 +24,42 @@ class GuildModel extends Model<GuildDataModel> implements GuildDataModel {
     public timezone_code: string;
     public timezone: string;
     public id: string;
+    
+    static async factory(sequelize: Sequelize): Promise<GuildModel> {
+        const attributes: SequelizeAttributes<GuildDataModel> = {
+            ponctual: {
+                type: DataTypes.ARRAY,
+            },
+            freq: {
+                type: DataTypes.ARRAY,
+            },
+            guild_owner_id: {
+                type: DataTypes.STRING,
+            },
+            token_expires: {
+                type: DataTypes.INTEGER,
+            },
+            token: {
+                type: DataTypes.STRING
+            },
+            id: {
+                type: DataTypes.STRING,
+            },
+            refresh_token: {
+                type: DataTypes.STRING,
+            },
+            timezone: {
+                type: DataTypes.STRING,
+            },
+            timezone_code: {
+                type: DataTypes.STRING
+            }
+        };
+      
+        const Guild = sequelize.define<GuildModel, GuildDataModel>('Guild', attributes);
+
+        return await Guild.sync();
+    }
 }
 
 export default GuildModel;  
--- src/models/MessageModel.ts
+++ src/models/MessageModel.ts
@@ -1,4 +1,5 @@
-import { Model } from "sequelize/types";
+import { DataTypes, Model, Sequelize } from "sequelize";
+import { SequelizeAttributes } from "src/types";
 
 interface MessageDataModel {
     id: string;
@@ -25,6 +26,51 @@ class MessageModel extends Model<MessageDataModel> implements MessageDataModel {
     public description: string;
     public sys_content: string;
     public type: MessageType.Frequential|MessageType.Ponctual;
+
+    
+    static async factory(sequelize: Sequelize): Promise<MessageModel> {
+        const attributes: SequelizeAttributes<MessageDataModel> = {
+            id: {
+                type: DataTypes.UUIDV4,
+                allowNull: false,
+                unique: true
+            },
+            channel_id: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            cron: {
+                type: DataTypes.STRING
+            },
+            timestamp: {
+                type: DataTypes.INTEGER
+            },
+            message: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            description: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            sys_content: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            type: {
+                type: DataTypes.ENUM,
+                allowNull: false,
+                values: [
+                    MessageType.Frequential.toString(),
+                    MessageType.Ponctual.toString(),
+                ]
+            }
+        };
+      
+        const Message = sequelize.define<MessageModel, MessageDataModel>('Message', attributes);
+
+        return await Message.sync();
+    }
 }
 
 export default MessageModel;
