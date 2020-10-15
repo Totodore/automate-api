3c1576c6245e9990af8c1ba78e40d085ce268006
totodore <prevottheodore@gmail.com>
2020-10-15 22:34:55 +0200

    update logger for LoadUserData

--- src/middlewares/LoadUserData.ts
+++ src/middlewares/LoadUserData.ts
@@ -1,8 +1,10 @@
 import { SessionRequest } from "../requests/RequestsMiddleware";
 import {Response} from "express";
 import fetch from "node-fetch";
+import Logger from "src/utils/Logger";
 
 export default async function(req: SessionRequest, res: Response, next: Function) {
+    const logger = new Logger("GetUserInfos");
     if (req.session.userId) {
         const userData = await req.getUser(req.session.userId);
         const reqUser = await fetch("https://discordapp.com/api/users/@me", {
@@ -12,7 +14,7 @@ export default async function(req: SessionRequest, res: Response, next: Function
             }
         });
         if (reqUser.status != 200) {
-            console.log(`Error : ${reqUser.status} ${reqUser.statusText}`);
+            logger.log(`Error : ${reqUser.status} ${reqUser.statusText}`);
             res.redirect("../connect?msg="+encodeURI("Whoops ! It seems like your connection to Discord is impossible!"));
             return;
 
