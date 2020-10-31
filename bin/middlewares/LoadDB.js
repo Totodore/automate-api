338417fd79bf2a2f58d1791357ba81951106aa34
totodore <prevottheodore@gmail.com>
2020-10-31 15:00:12 +0100

    removing bin folder

--- bin/middlewares/LoadDB.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function default_1(req, res, next) {
-    req.dbManager = req.app.get("dbManager");
-    next();
-}
-exports.default = default_1;
--- bin/middlewares/LoggerRequest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Logger_1 = require("../utils/Logger");
-function default_1(req, res, next) {
-    const logger = new Logger_1.default("Request");
-    logger.log(req.method, req.path, "requested");
-    next();
-}
-exports.default = default_1;
