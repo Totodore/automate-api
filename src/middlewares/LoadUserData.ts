5c83968084a8e45f66bade5ad4c1ec5f35266f51
totodore <prevottheodore@gmail.com>
2020-11-06 22:31:21 +0100

    try catch update

--- src/middlewares/LoadUserData.ts
+++ src/middlewares/LoadUserData.ts
@@ -6,24 +6,29 @@ import Logger from "../utils/Logger";
 export default async function(req: SessionRequest, res: Response, next: Function) {
     const logger = new Logger("LoadUserData");
     if (req.cookies.userId) {
-        const userData = await req.getUser(req.cookies.userId);
-        const reqUser = await fetch("https://discordapp.com/api/users/@me", {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${userData.access_token}`
-            }
-        });
-        if (reqUser.status != 200) {
-            logger.log(`Error : ${reqUser.status} ${reqUser.statusText}`);
-            res.redirect("../connect?msg="+encodeURI("Whoops ! It seems like your connection to Discord is impossible!"));
-            return;
+		try {
+			const userData = await req.getUser(req.cookies.userId);
+			const reqUser = await fetch("https://discordapp.com/api/users/@me", {
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': `Bearer ${userData.access_token}`
+				}
+			});
+			if (reqUser.status != 200) {
+				logger.log(`Error : ${reqUser.status} ${reqUser.statusText}`);
+				res.redirect("../connect?msg=" + encodeURI("Whoops ! It seems like your connection to Discord is impossible!"));
+				return;
 
-        }
-        const resUser = JSON.parse(await reqUser.text());
-        req.headerData = {
-            username: resUser.username,
-            avatar: `${process.env.CDN_ENDPOINT}/avatars/${req.cookies.userId}/${resUser.avatar}.png?size=64`
-        };
+			}
+			const resUser = JSON.parse(await reqUser.text());
+			req.headerData = {
+				username: resUser.username,
+				avatar: `${process.env.CDN_ENDPOINT}/avatars/${req.cookies.userId}/${resUser.avatar}.png?size=64`
+			};
+		} catch (e) {
+			logger.error(e);
+			res.redirect("../connect?msg=" + encodeURI("Whoops ! It seems like your connection to Discord is impossible!"));
+		}
     } 
     next();
 }
