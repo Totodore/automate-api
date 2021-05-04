edd2ab6af6603235eda26bed5d5c75f031484a6d
totodore <prevottheodore@gmail.com>
2021-05-04 16:07:09 +0100

    patching current profile decorator

--- src/decorators/current-profile.decorator.ts
+++ src/decorators/current-profile.decorator.ts
@@ -1,25 +1,40 @@
+import { AppLogger } from './../utils/app-logger.util';
 import { Profile } from 'passport-discord';
 import { User } from 'src/database/user.entity';
-import { createParamDecorator, SetMetadata, ExecutionContext } from '@nestjs/common';
+import { createParamDecorator, SetMetadata, ExecutionContext, InternalServerErrorException, HttpException } from '@nestjs/common';
 import { Request } from 'express';
 import { decode } from 'jsonwebtoken';
 import { GuildInfo } from 'passport-discord';
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const CurrentProfile = createParamDecorator(async (selectGuilds: boolean, context: ExecutionContext) => {
 
+  const logger = new AppLogger("CurrentProfile", false);
   const req: Request = context.switchToHttp().getRequest();
   const userId = decode(req.headers.authorization.substring(7)) as string;
   
   const user = req.user as User || await User.findOne(userId);
-  const profile = (await axios.get<Profile>(`${process.env.API_ENDPOINT}/users/@me`, {
-    headers: { Authorization: `Bearer ${user.token}` }
-  })).data;
-  if (selectGuilds) {
-    profile.guilds = (await axios.get<GuildInfo[]>(`${process.env.API_ENDPOINT}/users/@me/guilds`, {
+  let profile: Profile;
+  try {
+    profile = (await axios.get<Profile>(`${process.env.API_ENDPOINT}/users/@me`, {
       headers: { Authorization: `Bearer ${user.token}` }
     })).data;
+    if (selectGuilds) {
+      profile.guilds = (await axios.get<GuildInfo[]>(`${process.env.API_ENDPOINT}/users/@me/guilds`, {
+        headers: { Authorization: `Bearer ${user.token}` }
+      })).data;
+    }
+    req.user ??= user;
+    return profile;
+  } catch (e) {
+    const error = e as AxiosError;
+    logger.error(e, error.response.data.message, "retry in :",error.response.data.retry_after, "s");
+    if ((profile && !selectGuilds) || (profile.guilds && selectGuilds))
+      return profile;
+    else if (error.response.status === 429)
+      throw new HttpException("Discord API too many requests", 429);
+    else
+      throw new InternalServerErrorException();
   }
-  req.user ??= user;
-  return profile;
+  
 });
