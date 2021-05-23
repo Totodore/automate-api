d4c570df6bd0f7664e49b250c51bc669ea163632
totodore <prevottheodore@gmail.com>
2021-05-23 22:39:19 +0100

    controller patch

--- src/models/in/guild.in.model.ts
+++ src/models/in/guild.in.model.ts
@@ -1,32 +1,40 @@
 import { IsDateString, isDateString, IsString, Length, Matches, MaxLength } from "class-validator";
 
-export class DataMessageModel {
+export class PostFreqMessageInModel {
+  
+  @Matches(/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|([\d\*]+(\/|-)\d+)|\d+|\*) ?){5,7})/)
+  public cron: string;
+
   @MaxLength(2000)
   public message: string;
 
   @MaxLength(2000)
   public parsedMessage: string;
-}
 
-abstract class PostMessageModel extends DataMessageModel {
   @Length(18, 18)
   public channelId: string;
 
   @MaxLength(1000)
   public description: string;
-}
-
-export class PostFreqMessageInModel extends PostMessageModel {
-  
-  @Matches(/(@(annually|yearly|monthly|weekly|daily|hourly|reboot))|(@every (\d+(ns|us|µs|ms|s|m|h))+)|((((\d+,)+\d+|([\d\*]+(\/|-)\d+)|\d+|\*) ?){5,7})/)
-  public cron: string;
 
 }
 
-export class PostPonctMessageInModel extends PostMessageModel {
+export class PostPonctMessageInModel {
 
   @IsDateString()
   public date: string;
+
+  @Length(18, 18)
+  public channelId: string;
+
+  @MaxLength(1000)
+  public description: string;
+
+  @MaxLength(2000)
+  public message: string;
+
+  @MaxLength(2000)
+  public parsedMessage: string;
 }
 
 export class PatchFreqMessageInModel {
