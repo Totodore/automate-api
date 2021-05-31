2c1955127e6439b5b927966890b9682dfa73470f
totodore <prevottheodore@gmail.com>
2021-05-31 03:05:16 +0100

    oauth autorefresh token discord

--- src/models/oauth.model.ts
+++ src/models/oauth.model.ts
@@ -5,3 +5,10 @@ export type DiscordUser = [Profile, User];
 export interface DiscordOauthRequest {
   user: DiscordUser;
 }
+export interface TokenResponse {
+  access_token: string,
+  token_type: "Bearer",
+  expires_in: number,
+  refresh_token: string,
+  scope: string
+}
--- src/services/oauth.service.ts
+++ src/services/oauth.service.ts
@@ -1,14 +1,18 @@
-import { DiscordUser } from './../models/oauth.model';
+import { AppLogger } from './../utils/app-logger.util';
+import { DiscordUser, TokenResponse } from './../models/oauth.model';
 import { User } from './../database/user.entity';
-import { Injectable, HttpService } from '@nestjs/common';
+import { Injectable, HttpService, OnModuleInit } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, Profile } from "passport-discord";
+import { LessThan } from 'typeorm';
+import { AxiosResponse, AxiosError } from "axios";
 
 @Injectable()
-export class OauthService extends PassportStrategy(Strategy, 'discord') {
+export class OauthService extends PassportStrategy(Strategy, 'discord') implements OnModuleInit {
 
   constructor(
-    private readonly http: HttpService
+    private readonly http: HttpService,
+    private readonly logger: AppLogger
   ) {
     super({
       clientID: process.env.CLIENT_ID,
@@ -16,6 +20,12 @@ export class OauthService extends PassportStrategy(Strategy, 'discord') {
       callbackURL: process.env.CALLBACK_URL,
       scope: ["identify", "guilds"]
     });
+  }
+
+  public onModuleInit() {
+    setInterval(() => this.refreshToken(), 3600_000);
+    this.refreshToken();
+    this.logger.log("Oauth refresh token service started!");
   }
 
   public async validate(accessToken: string, refreshToken: string, profile: Profile): Promise<DiscordUser> {
@@ -34,5 +44,33 @@ export class OauthService extends PassportStrategy(Strategy, 'discord') {
       await user.save();
     }
     return [profile, user];
+  }
+
+  public async refreshToken() {
+    const users = await User.find({ tokenExpires: LessThan(new Date(Date.now() + 6 * 3600_000)) });
+    for (const user of users) {
+      try {
+        const res: AxiosResponse<TokenResponse> = await this.http.post(`https://discord.com/api/oauth2/token`, 
+          new URLSearchParams(Object.entries({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            grant_type: 'refresh_token',
+            refresh_token: user.refreshToken
+          }))
+        , { headers: { 'Content-Type': 'application/x-www-form-urlencoded'} }).toPromise();
+        if (res.status === 200) {
+          user.token = res.data.access_token;
+          user.refreshToken = res.data.refresh_token;
+          user.tokenExpires = new Date(Date.now() + res.data.expires_in * 1000);
+        } else {
+          this.logger.error("Could not refresh token for user:", user.name);
+        }
+        await user.save();
+      } catch (e) {
+        this.logger.error("Could not refresh token for user:", user.name);
+        this.logger.error((e as AxiosError).message);
+        console.error((e as AxiosError).response.data);
+      }
+    }
   }
 }
